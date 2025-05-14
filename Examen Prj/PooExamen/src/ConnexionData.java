2017b4c39ccf86dee41d36e252c393cc4a203f0f
TheLordRoukmout <hoelking1995@gmail.com>
2025-05-14 22:15:15 +0100

    Update prj

--- Examen Prj/PooExamen/src/ConnexionData.java
+++ Examen Prj/PooExamen/src/ConnexionData.java
@@ -2,12 +2,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnexionData {
-    private static final String URL = "jdbc:mysql://localhost:3306/tracktoys";
-    private static final String USER = "postgres";
-    private static final String PASS = "UHT808val201$";
+public class ConnexionData{
+    //Méthode pour se connecter a la db
+    public static Connection getConnexion() throws SQLException{
+        String url = "jdbc:sqlite:tracktoys.db";
+        return DriverManager.getConnection(url);
+    }
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASS);
+    //Méthode pour tester la connexion
+    public static void testConnection(){
+        try (Connection conn = getConnexion()){
+            System.out.println("Connection success");
+        }catch (SQLException e){
+            System.out.println("Connection error: " + e.getMessage());
+        }
     }
 }
--- Examen Prj/PooExamen/src/Main.java
+++ Examen Prj/PooExamen/src/Main.java
@@ -1,19 +1,8 @@
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
-        try (Connection conn = ConnexionData.getConnection();
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT * FROM clients")) {
-
-            while (rs.next()) {
-                System.out.println("Nom: " + rs.getString("nom"));
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        // Test de la connexion
+        ConnexionData.testConnection();
     }
 }
